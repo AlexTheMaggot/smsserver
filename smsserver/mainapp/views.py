aa5a5bcf9399abb3ecdb503b5281b35829827c27
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2020-12-21 21:21:55 +0500

    Add Contact CRUD views

--- smsserver/mainapp/views.py
+++ smsserver/mainapp/views.py
@@ -1,6 +1,6 @@
 # DjangoImports
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # End DjangoImports
 
@@ -8,6 +8,7 @@ from django.shortcuts import render, redirect
 # InternalImports
 
 from .main import send_sms
+from .models import Contact
 
 # End InternalImports
 
@@ -38,3 +39,49 @@ def success(request):
     return render(request, template)
 
 # End SuccessView
+
+
+# ContactCRUDViews
+
+def contact_add(request):
+    if request.method == 'GET':
+        template = 'mainapp/contact/contact_add.html'
+        return render(request, template)
+    elif request.method == 'POST':
+        contact = Contact(phone=request.POST['phone'])
+        contact.save()
+        url = 'contact_list'
+        return redirect(url)
+
+
+def contact_list(request):
+    contacts = Contact.objects.all().filter('id')
+    context = {
+        'contacts': contacts,
+    }
+    template = 'mainapp/contact/contact_list.html'
+    return render(request, template, context)
+
+
+def contact_edit(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    if request.method == 'GET':
+        context = {
+            'contact': contact,
+        }
+        template = 'mainapp/contact/contact_edit.html'
+        return render(request, template, context)
+    elif request.method == 'POST':
+        contact.phone = request.POST['phone']
+        contact.save()
+        url = 'contact_list'
+        return redirect(url)
+
+
+def contact_delete(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+    contact.delete()
+    url = 'contact_list'
+    return redirect(url)
+
+# End ContactCRUDViews
