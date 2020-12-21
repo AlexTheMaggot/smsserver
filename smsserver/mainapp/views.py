2e684d214f6d1308f198d2f55c237bcfb2344ce3
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2020-12-21 20:32:30 +0500

    Rewrite views.py (Add comments)

--- smsserver/mainapp/views.py
+++ smsserver/mainapp/views.py
@@ -1,18 +1,40 @@
+# DjangoImports
+
 from django.shortcuts import render, redirect
+
+# End DjangoImports
+
+
+# InternalImports
+
 from .main import send_sms
 
+# End InternalImports
+
+
+# IndexView
 
 def index(request):
     template = 'mainapp/index.html'
     return render(request, template)
 
+# End IndexView
+
+
+# SendSMSView
 
 def sending_sms(request):
     url = 'success'
     send_sms()
     return redirect(url)
 
+# End SendSMSView
+
+
+# SuccessView
 
 def success(request):
     template = 'mainapp/success.html'
     return render(request, template)
+
+# End SuccessView
