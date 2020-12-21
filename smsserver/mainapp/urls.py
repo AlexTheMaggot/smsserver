2e61224740e0ec1b0837bb1cde80009c4c3c1f66
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2020-12-21 20:42:51 +0500

    Rewrite urls.py. Add paths "Contact CRUD"

--- smsserver/mainapp/urls.py
+++ smsserver/mainapp/urls.py
@@ -1,8 +1,28 @@
+# DjangoImports
+
 from django.urls import path
+
+# End DjangoImports
+
+
+# InternalImports
+
 from . import views
+
+# End InternalImports
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('send_sms', views.sending_sms, name='send_sms'),
-    path('success', views.success, name='success'),
+    path('send_sms/', views.sending_sms, name='send_sms'),
+    path('success/', views.success, name='success'),
+
+    # ContactCRUD
+
+    path('contacts/add/', views.contact_add, name='contact_add'),
+    path('contacts/', views.contact_list, name='contact_list'),
+    path('contacts/<int:contact_id>/edit/', views.contact_edit, name='contact_edit'),
+    path('contacts/<int:contact_id>/delete/', views.contact_delete, name='contact_delete'),
+
+    # End ContactCRUD
+
 ]
